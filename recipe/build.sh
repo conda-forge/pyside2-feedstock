39b91c2cb04939b9c260503033ab771f85d1c006
Julien Schueller <schueller@phimeca.com>
2023-01-11 10:37:46 +0100

    Native shiboken build

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,9 +13,46 @@ set -ex
 sed -i.bak "s/, '-E'//g" sources/shiboken2/libshiboken/embed/embedding_generator.py
 sed -i.bak 's/${PYTHON_EXECUTABLE} -E/${PYTHON_EXECUTABLE}/g' sources/shiboken2/libshiboken/CMakeLists.txt
 
-# Use build shiboken2
-sed -i.bak "s/COMMAND Shiboken2::shiboken2/COMMAND shiboken2/g" sources/pyside2/cmake/Macros/PySideModules.cmake
-sed -i.bak "s/COMMAND Shiboken2::shiboken2/COMMAND shiboken2/g" sources/pyside2/tests/pysidetest/CMakeLists.txt
+if test "$CONDA_BUILD_CROSS_COMPILATION" = "1"
+then
+  # Use build shiboken2
+  sed -i.bak "s|COMMAND Shiboken2::shiboken2|COMMAND ${BUILD_PREFIX}/bin/shiboken2|g" sources/pyside2/cmake/Macros/PySideModules.cmake
+  sed -i.bak "s|COMMAND Shiboken2::shiboken2|COMMAND ${BUILD_PREFIX}/bin/shiboken2|g" sources/pyside2/tests/pysidetest/CMakeLists.txt
+fi
+
+pushd sources/shiboken2
+
+if test "$CONDA_BUILD_CROSS_COMPILATION" = "1"
+then
+  (
+    mkdir -p build_native && cd build_native
+
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    export PKG_CONFIG_PATH=${PKG_CONFIG_PATH//$PREFIX/$BUILD_PREFIX}
+    export CFLAGS=${CFLAGS//$PREFIX/$BUILD_PREFIX}
+    export CXXFLAGS=${CXXFLAGS//$PREFIX/$BUILD_PREFIX}
+
+    # hide host libs
+    mkdir -p $BUILD_PREFIX/${HOST}
+    mv $BUILD_PREFIX/${HOST} _hidden
+
+    cmake -LAH -G "Ninja" \
+      -DCMAKE_PREFIX_PATH=${BUILD_PREFIX} \
+      -DCMAKE_IGNORE_PREFIX_PATH="${PREFIX}" \
+      -DCMAKE_INSTALL_RPATH:STRING=${BUILD_PREFIX}/lib \
+      -DCMAKE_RANLIB=$BUILD_PREFIX/bin/${CONDA_TOOLCHAIN_BUILD}-ranlib \
+      -DCMAKE_INSTALL_PREFIX=${BUILD_PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_TESTS=OFF \
+      -DPYTHON_EXECUTABLE=${PYTHON} \
+    ..
+    cmake --build . --target install
+    mv _hidden $BUILD_PREFIX/${HOST}
+  )
+  rm -r build_native
+fi
 
 extra_cmake_flags=
 
@@ -30,7 +67,6 @@ else
   export RUN_TESTS=no
 fi
 
-pushd sources/shiboken2
 mkdir -p build && cd build
 
 # https://www.qt.io/blog/qt-on-apple-silicon
